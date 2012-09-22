2f6061cef2ff290fcb81a641fe0d83b4a43bdc0d
Chris Hughes <hughes.c.i@gmail.com>
2012-09-22 18:52:31 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cktls::Application.config.secret_token = '11c26290e6a3b7e2e25722f516fe8bee7e18ec026973402f9f43d0d7740ee76a126e81bd6c2bd1b931e0db5e81d39c7c5f7100391309fbf4a8f6ecc1cff59400'
