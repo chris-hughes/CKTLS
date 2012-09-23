910af2ba25a01f38be06b409441c6839d95b2dd4
Chris Hughes <hughes.c.i@gmail.com>
2012-09-23 19:12:20 +0100

    Add SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,15 +20,15 @@ Cktls::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  config.force_ssl = true
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
-
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
