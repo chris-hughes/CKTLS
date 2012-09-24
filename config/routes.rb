bdeaee358da77ee2b45bbe754372739e399f30b9
Chris Hughes <hughes.c.i@gmail.com>
2012-09-24 21:28:26 +0100

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,28 @@
 Cktls::Application.routes.draw do
   
-  root to: 'static_pages#home'
   
-  resources :users  
+  # static_pages - including root
+  root to: 'static_pages#home'
   match "/about", to: 'static_pages#about'
   match "/contact", to: 'static_pages#contact'
 
+  # users
+  resources :users
   match "/signup", to: 'users#new'
+
+  # signin
+  resources :sessions, only: [:new, :create, :destroy]
+  match "signin", to: 'sessions#new'
+  match "signout", to: 'sessions#destroy', via: :delete
+
+
+
+
+
+
+
+
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
