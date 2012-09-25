10c60021c08ae931d26aa928f18708e5e6ce98a0
Chris Hughes <hughes.c.i@gmail.com>
2012-09-25 21:22:46 +0100

    Users resource functionally complete

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -31,6 +31,7 @@ describe "Authentication" do
       		before { sign_in user } # sign_in is a user defined function in utilities
 
 		    it { should have_selector('title', text: user.name) }
+		    it { should have_link('Users',    href: users_path) }
 		    it { should have_link('Profile', href: user_path(user)) }
 		    it { should have_link('Settings', href: edit_user_path(user)) }
 		    it { should have_link('Sign out', href: signout_path) }
@@ -62,6 +63,11 @@ describe "Authentication" do
 
   					specify { response.should redirect_to(signin_path) }
   				end
+
+  				describe "visiting the user index" do
+          			before { visit users_path }
+          			it { should have_selector('title', text: 'Sign in') }
+		        end
 
   			end
 
@@ -98,6 +104,19 @@ describe "Authentication" do
   				specify { response.should redirect_to(root_path) }
   			end
   		end
+
+      describe "as non-admin user" do
+        let(:user) { FactoryGirl.create(:user) }
+        let(:non_admin) { FactoryGirl.create(:user) }
+
+        before { sign_in non_admin }
+
+        describe "submitting a DELETE request to the Users#destroy action" do
+          before { delete user_path(user) }
+          specify { response.should redirect_to(root_path) }        
+        end
+      end
+
   	end
 
 end
