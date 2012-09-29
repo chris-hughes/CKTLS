522b2300a5ce9c304bf9a8ac45f813e32ca92064
Chris Hughes <hughes.c.i@gmail.com>
2012-09-29 18:46:04 +0100

    Create new cocktails form

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -7,4 +7,18 @@ class CocktailsController < ApplicationController
 	def index
 	end
 
+	def new
+		@cocktail = Cocktail.new
+	end
+
+	def create
+		@cocktail = Cocktail.new(params[:cocktail])
+		if @cocktail.save
+			flash[:success] = @cocktail.name.titleize+" added to CKTLS!"
+			redirect_to cocktails_path
+		else
+			render 'new'
+		end
+	end
+
 end
--- spec/requests/cocktail_pages_spec.rb
+++ spec/requests/cocktail_pages_spec.rb
@@ -8,8 +8,40 @@ describe "UserPages" do
 		let(:cocktail) { FactoryGirl.create(:cocktail) }
 		before { visit cocktail_path(cocktail) }
 
-		it { should have_selector('h1',    text: cocktail.name) }
-		it { should have_selector('title', text: cocktail.name) }
+		it { should have_selector('h1',    text: cocktail.name.titleize) }
+		it { should have_selector('title', text: cocktail.name.titleize) }
 	end
-	
+
+	describe "New cocktail Page" do
+		before { visit new_cocktail_path }
+		it { should have_selector('h1', text:'Create Your Cocktail') }
+		it { should have_selector('title', text: 'CKTLS | Create') }
+	end
+
+	describe "Create new cocktail" do
+		before { visit new_cocktail_path }
+
+		let(:submit) { "Create my cocktail" }
+
+		describe "with invalid information" do
+			it "should not create a new cocktail" do
+				expect { click_button submit }.not_to change(Cocktail, :count)
+			end
+		end
+
+		describe "with valid information" do
+			before do
+				fill_in "Name",			with: "Example cocktail"
+				select "Fruity",		from: "Family"
+				fill_in "Makes",		with: 2
+				select "Martini glass", from: "Glass"
+				select "No",			from: "Chilled"
+			end
+
+			it 'should create a cocktail' do
+				expect { click_button submit }.to change(Cocktail, :count).by(1)
+			end
+		end
+	end
+
 end
